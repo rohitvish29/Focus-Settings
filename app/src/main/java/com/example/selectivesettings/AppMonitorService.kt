c55d6786244ca16ee70d4f1ba64ff153a0dd1988
rohitvish29 <130687645+rohitvish29@users.noreply.github.com>
2026-08-27 00:59:08 +0530

    Delete app directory

--- app/src/main/java/com/example/selectivesettings/AppMonitorService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.selectivesettings
-
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-
-class AppMonitorService : Service() {
-    override fun onBind(intent: Intent?): IBinder? = null
-}
